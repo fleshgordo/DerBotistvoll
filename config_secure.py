955eaf652b1052a8c8e9fd9eee23ab565fc5bb6d
Gordan Savicic <gordo@yugo.at>
2016-03-19 22:48:44 +0100

    setup file

--- config_secure.py
+++
@@ -1,6 +0,0 @@
-config = {
-	'consumer_key' : "aH2uQg4ta49gqM2cTwQReT2Uo",
-	'consumer_secret' : "Ror7TTREsKH7qzBGCaM0v2KTqvlTWfR1TUnWzghFkeaAyYMkmW",
-	'access_token' : "709360177884086272-uEaqXZ5eEHBrfOdV7uYiI1Eyj7q7pT0",
-	'access_token_secret' : "rZ3MJ0mvzZh1ZwOlUp4RBL6YX7dDfCbk4NdkqJ8lpLqiS"
-}
